be41beca182c131acd62b6e924a199609179ab89
User <joaovictorddm@gmail.com>
2025-10-22 18:59:29 -0300

    Commit de correção do list.js

--- src/menus/list.js
+++ src/menus/list.js
@@ -1,5 +1,5 @@
 import { taskManager } from "../gerenciador/tasks.js"
-import { log } from "@clack/prompts";
+import { isCancel, log } from "@clack/prompts";
 import { mainMenu } from "./main.js";
 import chalk from "chalk";
 
@@ -9,5 +9,21 @@ export async function listTasksMenu(){
         setTimeout(() => mainMenu(),2000);
     }
     
+    const selected = await select({
+        message: "Selecione uma tarefa",
+        options: [
+            ...taskManager.toArray().map(({ name, status })=>({
+                label: `${ taskManager.colorStatus(status) } ${chalk.white.underline}`,
+                value: name
+            })),
+            {label: "Menu Principal",
+             value: "main"}
+        ]
+    })
+    if(isCancel(selected) || selected  === 'main'){
+        mainMenu();
+        return;
+    }
+
 }
 
